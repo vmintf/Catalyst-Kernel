0989c034ffd9b912843789f876eeee481e191eb9
vmintf <vmintf@gmail.com>
2026-02-18 17:11:25 +0900

    chore: Add GPL license headers to source files

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,13 @@
+// Catalyst Kernel - A bare-metal UEFI kernel using Python DSL and Zig comptime AOT compilation.
+// Copyright (C) 2025  Skystarry.xyz
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// See LICENSE for details.
+
 const std = @import("std");
 const uefi = std.os.uefi;
 const gop = uefi.protocol.GraphicsOutput;
