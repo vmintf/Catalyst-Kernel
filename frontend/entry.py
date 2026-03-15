011f57177656a5ba0a7eec5340e535bbd75a2697
vmintf <vmintf@gmail.com>
2026-03-15 17:35:07 +0900

    docs: add GPL license headers to all source files
    - Include copyright year and license details in all files
    - Provide reference to LICENSE file for further information
    
    Signed-off-by: vmintf <vmintf@gmail.com>

--- frontend/entry.py
+++ frontend/entry.py
@@ -1,3 +1,13 @@
+# Catalyst Kernel - A bare-metal UEFI kernel using Python DSL and Zig AOT compilation.
+# Copyright (C) 2026  Skystarry.xyz
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# See LICENSE for details.
+
 from src.decorator import KernelDecorator, ShellCompiler
 from src.opcodes import *
 from src.nodes import *
